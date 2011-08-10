da9f76a8fb482ada40eb92e127219ebba394a2aa
Mark Walsh <mawalsh@csu.edu.au>
2011-08-10 16:55:35 +1000

    KERN-2146 create windows batch files similar to unix shell files tools/run_debug.sh and tools/version

---
+++ tools/run_debug.bat
@@ -0,0 +1,14 @@
+@echo off
+break off
+
+REM let variable K2VERSION exist only while this batch file runs.
+setlocal
+
+REM sets a value for variable K2VERSION
+call tools\version.bat
+
+if "%1" == "--suspend" (set suspend=y) else (set suspend=n)
+
+java -Xmx512m -XX:MaxPermSize=256m -server -Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=%suspend% -jar app/target/org.sakaiproject.nakamura.app-%K2VERSION%.jar -f - 
+
+endlocal
