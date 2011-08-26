eaaa35cf958edebe7a7c4df6d1e8004e43da6e56
John King <johnk@media.berkeley.edu>
2011-08-25 18:10:53 -0700

    KERN-2122 - fixing kern-2122.rb reference to full_group_creator.rb and first working version of removing content from managed groups

--- testscripts/SlingRuby/kerns/kern-2122.rb
+++ testscripts/SlingRuby/kerns/kern-2122.rb
@@ -1,11 +1,13 @@
 #!/usr/bin/env ruby
 # Add all files in testscripts\SlingRuby\lib directory to ruby "require" search path
-require 'ruby-lib-dir.rb'
 
+require './ruby-lib-dir.rb'
 require 'sling/test'
 require 'sling/file'
 require 'sling/users'
+require 'sling/full_group_creator'
 require 'test/unit.rb'
+
 include SlingUsers
 include SlingFile
 
@@ -17,45 +19,59 @@ class TC_Kern2122 < Test::Unit::TestCase
     super
     @fm = FileManager.new(@s)
     @um = UserManager.new(@s)
+    @full_group_creator = SlingUsers::FullGroupCreator.new @s
+    @full_group_creator.log.level = Logger::INFO
   end
 
   def test_add_and_remove_viewers
     m = Time.now.to_i.to_s
 
     # Create some users
-    manager = create_user("manager-#{m}")
+    # manager = create_user("manager-#{m}")
     viewer1 = create_user("viewer1-#{m}")
     viewer2 = create_user("viewer2-#{m}")
 
-    @s.switch_user(manager)
-    content = Time.now.to_f
-    name = "random-#{content}.txt"
-    fileBody = "Add the time to make it sort of random #{Time.now.to_f}."
-    upload_res = @fm.upload_pooled_file(name, fileBody, 'text/plain')
+    @s.switch_user(viewer1)
+    ticks = Time.now.to_f
+
+    managed_group = @full_group_creator.create_full_group viewer1.name, "group-#{m}", "kern-2122 int test group", "kern-2122 int test group description"
+    assert_not_nil(managed_group, "expected managed group to be created")
+
+    file_name = "random-#{ticks}.txt"
+    file_body = "Add the time to make it sort of random #{Time.now.to_f}."
+    upload_res = @fm.upload_pooled_file(file_name, file_body, 'text/plain')
     json = JSON.parse(upload_res.body)
-    file_id = json[name]["poolId"]
+    file_id = json[file_name]["poolId"]
 
     wait_for_indexer()
     find_res = @s.execute_get(@s.url_for("/var/search/pool/me/manager-all.tidy.json"))
     assert_equal("200",find_res.code,find_res.body)
 
-    add_viewers_res = @fm.manage_members(file_id, [viewer1.name, viewer2.name], nil, nil, nil)
+    add_viewers_res = @fm.manage_members(file_id, [viewer1.name, viewer2.name, managed_group.name], nil, nil, nil)
     assert_equal("200",add_viewers_res.code, "manage_members add viewers POST should have succeeded")
     members_res = @fm.get_members file_id
     json = JSON.parse(members_res.body)
     assert_equal("200",members_res.code, "get_members GET should have succeeded")
     viewers = json["viewers"]
-    assert_equal(2, viewers.length, "should be 2 viewers added by manager" )
+    assert_equal(3, viewers.length, "should be 3 viewers added by manager" )
 
     # now have viewer1 try to remove viewer2
-    @s.switch_user(viewer1)
+    # @s.switch_user(viewer1)
     remove_viewer2_res = @fm.manage_members(file_id, nil, [viewer2.name], nil, nil)
     members_res = @fm.get_members file_id
     json = JSON.parse(members_res.body)
     assert_equal("200",members_res.code)
     viewers = json["viewers"]
-    assert_equal(2, viewers.length, "should be 2 viewers because viewer1 cannot remove viewer2" )
+    assert_equal(3, viewers.length, "should be 3 viewers because viewer1 cannot remove viewer2" )
 
+    # now have viewer1 remove managed group
+    # this is how to remove an item from a user's library
+    remove_group_res = @fm.manage_members(file_id, nil, [managed_group.name], nil, nil)
+    assert_equal("200",remove_group_res.code, "expected viewer1 to be able to remove a group managed by them")
+    members_res = @fm.get_members file_id
+    assert_equal("200",members_res.code)
+    json = JSON.parse(members_res.body)
+    viewers = json["viewers"]
     # now have viewer1 remove themself
     # this is how to remove an item from a user's library
     remove_viewer1_res = @fm.manage_members(file_id, nil, [viewer1.name], nil, nil)
