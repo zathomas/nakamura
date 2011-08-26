cf5d6d98cc87e4d36b6fffc1d63501b8816f4dd1
John King <johnk@media.berkeley.edu>
2011-08-26 14:47:32 -0700

    KERN-2122 - added rep:group-managers to AuthorizableIndexingHandler group properties to be indexed by solr in manager field, changed solr search in ManageMembersContentPoolServlet.findMyManageGroups to seach manager field directly, cleaned up kern-2122.rb int test

--- testscripts/SlingRuby/kerns/kern-2122.rb
+++ testscripts/SlingRuby/kerns/kern-2122.rb
@@ -40,23 +40,24 @@ class TC_Kern2122 < Test::Unit::TestCase
     file_name = "random-#{ticks}.txt"
     file_body = "Add the time to make it sort of random #{Time.now.to_f}."
     upload_res = @fm.upload_pooled_file(file_name, file_body, 'text/plain')
+    assert_equal("201",upload_res.code,"should have succeeded in uploading file #{file_name}")
     json = JSON.parse(upload_res.body)
     file_id = json[file_name]["poolId"]
 
     wait_for_indexer()
     find_res = @s.execute_get(@s.url_for("/var/search/pool/me/manager-all.tidy.json"))
-    assert_equal("200",find_res.code,find_res.body)
+    assert_equal("200",find_res.code,"should have succeeded in finding pool file #{file_id}")
 
+    # add 3 viewers including the group that viewer1 manages
     add_viewers_res = @fm.manage_members(file_id, [viewer1.name, viewer2.name, managed_group.name], nil, nil, nil)
     assert_equal("200",add_viewers_res.code, "manage_members add viewers POST should have succeeded")
     members_res = @fm.get_members file_id
     json = JSON.parse(members_res.body)
     assert_equal("200",members_res.code, "get_members GET should have succeeded")
     viewers = json["viewers"]
-    assert_equal(3, viewers.length, "should be 3 viewers added by manager" )
+    assert_equal(3, viewers.length, "should be 3 viewers added by viewer1" )
 
     # now have viewer1 try to remove viewer2
-    # @s.switch_user(viewer1)
     remove_viewer2_res = @fm.manage_members(file_id, nil, [viewer2.name], nil, nil)
     members_res = @fm.get_members file_id
     json = JSON.parse(members_res.body)
@@ -65,7 +66,7 @@ class TC_Kern2122 < Test::Unit::TestCase
     assert_equal(3, viewers.length, "should be 3 viewers because viewer1 cannot remove viewer2" )
 
     # now have viewer1 remove managed group
-    # this is how to remove an item from a user's library
+    # this is how to remove an item from the library of a group managed by a user
     remove_group_res = @fm.manage_members(file_id, nil, [managed_group.name], nil, nil)
     assert_equal("200",remove_group_res.code, "expected viewer1 to be able to remove a group managed by them")
     members_res = @fm.get_members file_id
