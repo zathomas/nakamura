32fd06ef5a91c66f9bf4df8e25d1135f87a60efc
Chris Tweney <chris@media.berkeley.edu>
2011-10-06 09:57:04 -0700

    KERN-2147 If reindexAll is true, also reindex all content and authorizables in the Solr indexes

--- bundles/upgrade/src/main/java/org/sakaiproject/nakamura/upgrade/servlet/SparseUpgradeServlet.java
+++ bundles/upgrade/src/main/java/org/sakaiproject/nakamura/upgrade/servlet/SparseUpgradeServlet.java
@@ -27,14 +27,13 @@ import org.apache.sling.api.servlets.SlingAllMethodsServlet;
 import org.sakaiproject.nakamura.api.doc.BindingType;
 import org.sakaiproject.nakamura.api.doc.ServiceBinding;
 import org.sakaiproject.nakamura.api.doc.ServiceDocumentation;
-import org.sakaiproject.nakamura.api.doc.ServiceExtension;
 import org.sakaiproject.nakamura.api.doc.ServiceMethod;
 import org.sakaiproject.nakamura.api.doc.ServiceParameter;
 import org.sakaiproject.nakamura.api.doc.ServiceResponse;
-import org.sakaiproject.nakamura.api.doc.ServiceSelector;
 import org.sakaiproject.nakamura.api.lite.Feedback;
 import org.sakaiproject.nakamura.api.lite.MigrateContentService;
 import org.sakaiproject.nakamura.api.lite.Session;
+import org.sakaiproject.nakamura.api.lite.StorageClientException;
 import org.sakaiproject.nakamura.api.lite.StorageClientUtils;
 import org.sakaiproject.nakamura.api.lite.authorizable.AuthorizableManager;
 import org.sakaiproject.nakamura.api.lite.authorizable.User;
@@ -47,26 +46,30 @@ import java.text.MessageFormat;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletResponse;
 
-@ServiceDocumentation(name="Sparse Upgrade Servlet", okForVersion = "0.11",
-    description="Upgrades data stored in sparsemapcontent storage layer by running all the PropertyMigrator instances " +
-        "that are registered. Note that the upgrade only works for JDBC storage clients. The upgrade may take a long " +
-        "time to run; progress of the upgrade gets written to the response every so often and can be seen in realtime if " +
-        "your client supports buffered HTTP requests. Use -N option in curl to enable this. Your system log will have " +
-        "more detail on the progress of the upgrade.",
-    shortDescription="Upgrades data stored in sparsemapcontent",
-    bindings=@ServiceBinding(type = BindingType.PATH, bindings = "/system/sparseupgrade"),
-    methods=@ServiceMethod(name="POST",
-        description={"Upgrades data stored in sparsemapcontent",
-            "Example<br>" +
-            "<pre>curl -N -u admin:password -e http://localhost:8080 -FdryRun=false -FreindexAll=true -Flimit=5000 http://localhost:8080/system/sparseupgrade</pre>"},
-        parameters={
-        @ServiceParameter(name="dryRun", description="Whether this is a dry run or not; in dry run, no data is actually saved. Default=true."),
-        @ServiceParameter(name="limit",description="If dryRun is true, then process up to this many rows; no effect if dryRun is false. Default=Integer.MAX_VALUE (2147483647)."),
-        @ServiceParameter(name="reindexAll",description="If true, then reindex every row whether it's changed or not. Makes the upgrade take longer. Default=false.")
-        },
-        response={
-          @ServiceResponse(code=200,description="Success, the upgrade ran with no issues."),
-          @ServiceResponse(code=500,description="Failure with HTML explanation.")}
+@ServiceDocumentation(name = "Sparse Upgrade Servlet", okForVersion = "0.11",
+        description = "Upgrades data stored in sparsemapcontent storage layer by running all the PropertyMigrator instances " +
+                "that are registered. Note that the upgrade only works for JDBC storage clients. The upgrade may take a long " +
+                "time to run; progress of the upgrade gets written to the response every so often and can be seen in realtime if " +
+                "your client supports buffered HTTP requests. Use -N option in curl to enable this. Your system log will have " +
+                "more detail on the progress of the upgrade.",
+        shortDescription = "Upgrades data stored in sparsemapcontent",
+        bindings = @ServiceBinding(type = BindingType.PATH, bindings = "/system/sparseupgrade"),
+        methods = @ServiceMethod(name = "POST",
+                description = {"Upgrades data stored in sparsemapcontent",
+                        "Example<br>" +
+                                "<pre>curl -N -u admin:password -e http://localhost:8080 -FdryRun=false -FreindexAll=true -Flimit=5000 " +
+                                "http://localhost:8080/system/sparseupgrade</pre>"},
+                parameters = {
+                        @ServiceParameter(name = "dryRun", description = "Whether this is a dry run or not; in dry run, no data is actually " +
+                                "saved. Default=true."),
+                        @ServiceParameter(name = "limit", description = "If dryRun is true, then process up to this many rows; no effect if " +
+                                "dryRun is false. Default=Integer.MAX_VALUE (2147483647)."),
+                        @ServiceParameter(name = "reindexAll", description = "If true, then reindex every row whether it's changed or not. " +
+                                "This will also force reindexing of every row in Solr. Makes the upgrade take longer. Default=false.")
+                },
+                response = {
+                        @ServiceResponse(code = 200, description = "Success, the upgrade ran with no issues."),
+                        @ServiceResponse(code = 500, description = "Failure with HTML explanation.")}
         ))
 @SlingServlet(paths = {"/system/sparseupgrade"}, generateComponent = true, generateService = true, methods = {"POST"})
 public class SparseUpgradeServlet extends SlingAllMethodsServlet {
@@ -115,6 +118,14 @@ public class SparseUpgradeServlet extends SlingAllMethodsServlet {
       // do the actual migration
       this.migrationService.migrate(dryRun, limit, reindexAll, getFeedback(response));
 
+      // reindex solr if necessary
+      if (reindexAll && !dryRun) {
+        msg = "Reindexing all content and authorizables in Solr...";
+        writeToResponse(msg, response);
+        LOGGER.info(msg);
+        reindexSolr(request);
+      }
+
     } catch (Exception e) {
       LOGGER.error("Got exception processing sparse upgrade", e);
       response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
@@ -143,6 +154,12 @@ public class SparseUpgradeServlet extends SlingAllMethodsServlet {
       }
 
     };
+  }
+
+  private void reindexSolr(SlingHttpServletRequest request) throws StorageClientException {
+    Session session = StorageClientUtils.adaptToSession(request.getResourceResolver().adaptTo(javax.jcr.Session.class));
+    session.getContentManager().triggerRefreshAll();
+    session.getAuthorizableManager().triggerRefreshAll();
   }
 
   private void writeToResponse(String msg, SlingHttpServletResponse response) {
