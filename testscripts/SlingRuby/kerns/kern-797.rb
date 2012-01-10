0ea336b0873b834037c4da5d11b651891152b658
Zach A. Thomas <zach@aeroplanesoftware.com>
2012-01-10 15:04:49 -0600

    KERN-2501 fix kern-797.rb, /tags no longer behaves like a JCR node

--- testscripts/SlingRuby/kerns/kern-797.rb
+++ testscripts/SlingRuby/kerns/kern-797.rb
@@ -75,22 +75,9 @@ class TC_MyFileTest_797 < Test::Unit::TestCase
     assert_equal(false, json["/var"])
     assert_not_nil(json["privileges"])
     assert_equal(true, json["privileges"]["jcr:read"])
-    
-    # verify behavior against a regular resource (i.e. not pooled content)
 
     # verbose output
     @s.switch_user(SlingUsers::User.admin_user())
-    # this used to use / (the root node) as the path to check
-    # but now that we use sparsemapcontent, / resolves to sparse content
-    url = "/tags.canModify.json?verbose=true";
-    resp = @s.execute_get(@s.url_for(url));
-    assert_equal(200, resp.code.to_i, "Should be OK");
-    json = JSON.parse(resp.body)
-    # admin can modify /
-    @log.info(json)
-    assert_equal(true, json["/tags"])
-    assert_not_nil(json["privileges"])
-    assert_equal(true, json["privileges"]["jcr:all"])
     
     # verify 404 not found
     url = "/p/foo.canModify.json?verbose=true";
