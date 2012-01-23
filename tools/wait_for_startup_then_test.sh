27d967c33681c0396c0720aa650d42cd635b22cb
Zach A. Thomas <zach@aeroplanesoftware.com>
2012-01-23 11:23:18 -0600

    give shutdown the correct sling.home setting. Add timeout capability to wait_for_startup script.

--- tools/wait_for_startup_then_test.sh
+++ tools/wait_for_startup_then_test.sh
@@ -5,8 +5,17 @@ if [ $# -lt 1 ] ; then
   exit 0
 fi
 echo "Waiting for startup..."
-while ! [ -e $1 ];do sleep 2; done
-while ! grep "org.sakaiproject.nakamura.world BundleEvent STARTED" $1 1> /dev/null ;do sleep 2; done
+TIMEOUT=150
+TIMER=0
+while ! [ $TIMER -gt $TIMEOUT ] && ! [ -e $1 ] ;do sleep 2; TIMER=$((TIMER+1)); done
+while ! [ $TIMER -gt $TIMEOUT ] && ! grep "org.sakaiproject.nakamura.world BundleEvent STARTED" $1 1> /dev/null ;do sleep 2; TIMER=$((TIMER+1)); done
+
+if [ $TIMER -gt $TIMEOUT ]
+then
+    echo "Startup did not complete for a long time. Giving up."
+    exit 1
+fi
+
 echo "Startup complete..."
 ./tools/runalltests-with-exit-code.sh
 
