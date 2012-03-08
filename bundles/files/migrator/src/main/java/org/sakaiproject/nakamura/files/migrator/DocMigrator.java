4e6e672a7ba104d91976f7092b8e76af81a1cb49
Zach A. Thomas <zach@aeroplanesoftware.com>
2012-03-08 16:05:44 -0600

    KERN-2625 improve the formatting of migration output

--- bundles/files/migrator/src/main/java/org/sakaiproject/nakamura/files/migrator/DocMigrator.java
+++ bundles/files/migrator/src/main/java/org/sakaiproject/nakamura/files/migrator/DocMigrator.java
@@ -48,6 +48,7 @@ import java.util.Iterator;
 public class DocMigrator implements FileMigrationService {
   private static final Logger LOGGER = LoggerFactory.getLogger(DocMigrator.class);
   private static final String EMPTY_DIV = "<div />";
+  private static final String STRUCTURE_ZERO = FilesConstants.STRUCTURE_FIELD_STEM + "0";
 
   @Reference
   protected Repository repository;
@@ -73,7 +74,7 @@ public class DocMigrator implements FileMigrationService {
   protected void ensureRowPresent(JSONObject page) throws JSONException {
     JSONArray rows = page.getJSONArray("rows");
     if (rows == null || rows.length() < 1) {
-      page.append("rows", generateEmptyRow(1));
+      page.put("rows", generateEmptyRow(1));
     }
   }
   
@@ -83,7 +84,7 @@ public class DocMigrator implements FileMigrationService {
       JSONObject element = new JSONObject();
       element.put("id", columnId);
       element.put("type", type);
-      row.getJSONArray("columns").getJSONObject(column).append("elements", element);
+      row.getJSONArray("columns").getJSONObject(column).accumulate("elements", element);
       if (widgetData != null) {
         page.put(columnId, widgetData);
       }
@@ -102,7 +103,7 @@ public class DocMigrator implements FileMigrationService {
       }
     }
     if (rowHasContent) {
-      page.append("rows", row);
+      page.accumulate("rows", row);
     }
 
     return generateEmptyRow(columnsForNextRow > 0 ? columnsForNextRow : 1);
@@ -130,7 +131,6 @@ public class DocMigrator implements FileMigrationService {
     for (Iterator<String> keyIterator = subtree.keys(); keyIterator.hasNext();) {
       String key = keyIterator.next();
       if (key.startsWith("_")) {
-        newStructure.put(key, subtree.get(key));
         continue;
       }
       JSONObject structureItem = subtree.getJSONObject(key);
@@ -194,8 +194,14 @@ public class DocMigrator implements FileMigrationService {
       }
       processStructure0(structureItem, originalStructure, newStructure);
     }
-    // the finishing touch
-    newStructure.put(FilesConstants.SCHEMA_VERSION, 2);
+  }
+  
+  protected JSONObject createNewPageStructure(JSONObject structure0, JSONObject originalDoc) throws JSONException {
+    JSONObject newDoc = new JSONObject(originalDoc.toString());
+    processStructure0(structure0, originalDoc, newDoc);
+    // the finishing touches
+    newDoc.put(FilesConstants.SCHEMA_VERSION, 2);
+    return newDoc;
   }
   
   protected JSONObject getJSONObjectOrNull(JSONObject jsonObject, String key) throws JSONException {
@@ -233,14 +239,14 @@ public class DocMigrator implements FileMigrationService {
   }
 
   private boolean isNotSakaiDoc(Content content) {
-    return !content.hasProperty("structure0");
+    return !content.hasProperty(STRUCTURE_ZERO);
   }
 
   private boolean contentHasUpToDateStructure(Content content) throws SakaiDocMigrationException {
     Session adminSession = null;
     try {
       adminSession = repository.loginAdministrative();
-      JSONObject structure0 = new JSONObject((String)content.getProperty("structure0"));
+      JSONObject structure0 = new JSONObject((String)content.getProperty(STRUCTURE_ZERO));
       return !requiresMigration(structure0, content, adminSession.getContentManager());
     } catch (Exception e) {
       throw new SakaiDocMigrationException();
@@ -262,7 +268,7 @@ public class DocMigrator implements FileMigrationService {
 
   @Override
   public void migrateFileContent(Content content) {
-    if (content == null || !content.hasProperty("structure0")) {
+    if (content == null || !content.hasProperty(STRUCTURE_ZERO)) {
       return;
     }
     LOGGER.debug("Starting migration of {}", content.getPath());
@@ -272,8 +278,8 @@ public class DocMigrator implements FileMigrationService {
     try {
       ExtendedJSONWriter.writeContentTreeToWriter(stringJsonWriter, content, false,  -1);
       adminSession = repository.loginAdministrative();
-      JSONObject newPageStructure = new JSONObject();
-      processStructure0(new JSONObject((String) content.getProperty("structure0")), new JSONObject(stringWriter.toString()), newPageStructure);
+      JSONObject newPageStructure = createNewPageStructure(new JSONObject((String) content.getProperty(STRUCTURE_ZERO)), new JSONObject(stringWriter.toString()));
+
       JSONObject convertedStructure = (JSONObject) convertArraysToObjects(newPageStructure);
       validateStructure(convertedStructure);
       LOGGER.debug("Generated new page structure. Saving content {}", content.getPath());
@@ -297,30 +303,36 @@ public class DocMigrator implements FileMigrationService {
       JSONObject jsonObject = (JSONObject)json;
       for (Iterator<String> keyIterator = jsonObject.keys(); keyIterator.hasNext();) {
         String key = keyIterator.next();
-        if (jsonObject.get(key) instanceof JSONArray) {
+        if (objectIsArrayOfJSONObject(jsonObject.get(key))) {
           jsonObject.put(key, convertArrayToObject((JSONArray)jsonObject.get(key)));
         } else if (jsonObject.get(key) instanceof JSONObject) {
           jsonObject.put(key, convertArraysToObjects(jsonObject.get(key)));
         }
       }
       return jsonObject;
-    } else if (json instanceof JSONArray) {
+    } else if (objectIsArrayOfJSONObject(json)) {
       return convertArrayToObject((JSONArray)json);
     } else {
       return json;
     }
   }
-  
+
+  private boolean objectIsArrayOfJSONObject(Object json) throws JSONException {
+    return json instanceof JSONArray && ((JSONArray)json).length() > 0 && 
+      (((JSONArray)json).get(0) instanceof JSONObject || ((JSONArray)json).get(0) instanceof JSONArray);
+  }
+
   protected JSONObject convertArrayToObject(JSONArray jsonArray) throws JSONException {
     JSONObject arrayObject = new JSONObject();
     for(int i = 0; i < jsonArray.length(); i++) {
+      Object value = convertArraysToObjects(jsonArray.get(i));
       arrayObject.put("__array__" + i + "__", convertArraysToObjects(jsonArray.get(i)));
     }
     return arrayObject;
   }
 
   protected void validateStructure(JSONObject newPageStructure) throws JSONException, SakaiDocMigrationException {
-    if (!newPageStructure.has(FilesConstants.SCHEMA_VERSION)) {
+    if (!newPageStructure.has(FilesConstants.SCHEMA_VERSION) || !newPageStructure.has(STRUCTURE_ZERO)) {
       throw new SakaiDocMigrationException();
     }
     LOGGER.debug("new page structure passes validation.");
