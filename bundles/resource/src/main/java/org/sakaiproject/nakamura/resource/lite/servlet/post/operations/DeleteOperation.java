b60d03ecf648afc20f96be2d9092c96fffb6a4f8
Savitha Prakash <savithap@umich.edu>
2012-03-28 11:15:30 -0400

    KERN-2714: Deleting content from system, does not reset the Content count

--- bundles/resource/src/main/java/org/sakaiproject/nakamura/resource/lite/servlet/post/operations/DeleteOperation.java
+++ bundles/resource/src/main/java/org/sakaiproject/nakamura/resource/lite/servlet/post/operations/DeleteOperation.java
@@ -17,9 +17,16 @@
  */
 package org.sakaiproject.nakamura.resource.lite.servlet.post.operations;
 
+import com.google.common.collect.Sets;
+
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.Service;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.servlets.HtmlResponse;
+import org.apache.sling.commons.osgi.PropertiesUtil;
 import org.apache.sling.servlets.post.Modification;
 import org.sakaiproject.nakamura.api.lite.StorageClientException;
 import org.sakaiproject.nakamura.api.lite.StorageClientUtils;
@@ -27,19 +34,31 @@ import org.sakaiproject.nakamura.api.lite.accesscontrol.AccessDeniedException;
 import org.sakaiproject.nakamura.api.lite.content.Content;
 import org.sakaiproject.nakamura.api.lite.content.ContentManager;
 import org.sakaiproject.nakamura.api.resource.lite.AbstractSparsePostOperation;
+import org.sakaiproject.nakamura.api.resource.lite.SparsePostOperation;
+import org.sakaiproject.nakamura.api.user.AuthorizableCountChanger;
+import org.sakaiproject.nakamura.api.user.UserConstants;
 
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletResponse;
 
+@Component
+@Service(value = SparsePostOperation.class)
+@Property(name = "sling.post.operation", value = "delete")
 public class DeleteOperation extends AbstractSparsePostOperation {
+
+  @Reference
+  protected AuthorizableCountChanger authorizableCountChanger;
 
   @Override
   protected void doRun(SlingHttpServletRequest request, HtmlResponse response,
       ContentManager contentManager, List<Modification> changes, String contentPath)
-      throws StorageClientException, AccessDeniedException, IOException {
+          throws StorageClientException, AccessDeniedException, IOException {
+
 
     Iterator<Resource> res = getApplyToResources(request);
     if (res == null) {
@@ -49,6 +68,20 @@ public class DeleteOperation extends AbstractSparsePostOperation {
         response.setStatus(HttpServletResponse.SC_NOT_FOUND, "Missing source contentPath for delete");
         return;
       }
+
+      if(resource.isResourceType("sakai/pooled-content")){
+        //notify to reset content count
+        Content node = resource.adaptTo(Content.class);
+        Map<String, Object> properties = node.getProperties();
+        String[] managers = PropertiesUtil.toStringArray(properties.get("sakai:pooled-content-manager"));
+        String[] editors = PropertiesUtil.toStringArray(properties.get("sakai:pooled-content-editor"));
+        String[] viewers = PropertiesUtil.toStringArray(properties.get("sakai:pooled-content-viewer"));
+        Set<String> managerSet = Sets.newHashSet(managers);
+        Set<String> editorSet = Sets.newHashSet(editors);
+        Set<String> viewerSet = Sets.newHashSet(viewers);
+        this.authorizableCountChanger.notify(UserConstants.CONTENT_ITEMS_PROP, managerSet, editorSet, viewerSet);
+      }
+
       StorageClientUtils.deleteTree(contentManager, contentPath);
       changes.add(Modification.onDeleted(resource.getPath()));
 
