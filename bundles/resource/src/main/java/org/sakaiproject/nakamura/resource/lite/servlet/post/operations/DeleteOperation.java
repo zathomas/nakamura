f8e2bb8b1de9345610bb58b8f9c7b26d83d877a3
Zach A. Thomas <zach@aeroplanesoftware.com>
2012-06-06 10:57:50 -0500

    KERN-2893 return empty array intead of null to avoid NullPointerException

--- bundles/resource/src/main/java/org/sakaiproject/nakamura/resource/lite/servlet/post/operations/DeleteOperation.java
+++ bundles/resource/src/main/java/org/sakaiproject/nakamura/resource/lite/servlet/post/operations/DeleteOperation.java
@@ -85,9 +85,9 @@ public class DeleteOperation extends AbstractSparsePostOperation {
         //notify to reset content count
         Content node = resource.adaptTo(Content.class);
         Map<String, Object> properties = node.getProperties();
-        String[] managers = PropertiesUtil.toStringArray(properties.get("sakai:pooled-content-manager"));
-        String[] editors = PropertiesUtil.toStringArray(properties.get("sakai:pooled-content-editor"));
-        String[] viewers = PropertiesUtil.toStringArray(properties.get("sakai:pooled-content-viewer"));
+        String[] managers = PropertiesUtil.toStringArray(properties.get("sakai:pooled-content-manager"), new String[0]);
+        String[] editors = PropertiesUtil.toStringArray(properties.get("sakai:pooled-content-editor"), new String[0]);
+        String[] viewers = PropertiesUtil.toStringArray(properties.get("sakai:pooled-content-viewer"), new String[0]);
         
         Set<String> directAuthorizables = new HashSet<String>();
         directAuthorizables.addAll(Arrays.asList(managers));
