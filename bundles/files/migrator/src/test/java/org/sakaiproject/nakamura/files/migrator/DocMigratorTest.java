e425360dbe30523c54e91089faa74428436b275f
Zach A. Thomas <zach@aeroplanesoftware.com>
2012-08-14 16:56:56 -0500

    KERN-3122 additional processing for htmlblock regions to prune out unwanted widget elements

--- bundles/files/migrator/src/test/java/org/sakaiproject/nakamura/files/migrator/DocMigratorTest.java
+++ bundles/files/migrator/src/test/java/org/sakaiproject/nakamura/files/migrator/DocMigratorTest.java
@@ -361,4 +361,23 @@ public class DocMigratorTest extends Assert {
     assertFalse(docMigrator.requiresMigration(groupHome, null, null));
   }
 
+  @Test
+  public void testRemovesUnwantedWidgetIcon() throws Exception {
+    JSONObject doc = readJSONFromFile("UnwantedWidgetIcon.json");
+    JSONObject migrated = docMigrator.createNewPageStructure(
+        new JSONObject(doc.getString("structure0")), doc);
+    String htmlBlockId = migrated.getJSONObject("id8933623")
+        .getJSONArray("rows")
+        .getJSONObject(0)
+        .getJSONArray("columns")
+        .getJSONObject(0)
+        .getJSONArray("elements")
+        .getJSONObject(0).getString("id");
+    String htmlBlockContent = migrated.getJSONObject("id8933623")
+        .getJSONObject(htmlBlockId)
+        .getJSONObject("htmlblock")
+        .getString("content");
+    assertTrue(htmlBlockContent.indexOf("widget_inline") < 0);
+  }
+
 }
